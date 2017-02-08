9728d7aed56d281e0bcea9e865699d5a787747da
mihanic89 <yakovlev.mihail@gmail.com>
2017-02-08 23:48:07 +0300

    добавление аудиоплеера

---
+++ app/src/main/java/com/yamilab/lullababy/Audio.java
@@ -0,0 +1,48 @@
+package com.yamilab.lullababy;
+
+import java.io.Serializable;
+
+/**
+ * Created by Misha on 08.02.2017.
+ */
+
+public class Audio implements Serializable {
+
+    private Integer data;
+    private String album;
+    private String artist;
+
+    public Audio(Integer data, String album, String artist) {
+        this.data = data;
+        this.album = album;
+        this.artist = artist;
+    }
+
+    public Integer getData() {
+        return data;
+    }
+
+    public void setData(Integer data) {
+        this.data = data;
+    }
+
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+
+}
+
--- app/src/main/java/com/yamilab/lullababy/MainActivity.java
+++ app/src/main/java/com/yamilab/lullababy/MainActivity.java
@@ -2,9 +2,14 @@ package com.yamilab.lullababy;
 
 
 import android.animation.ObjectAnimator;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
@@ -13,12 +18,21 @@ import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String Broadcast_PLAY_NEW_AUDIO = "com.yamilab.lullababy.PlayNewAudio";
+
     String[] data_time;
     String[] data_melody;
-    private ImageButton btnStart,btnStop,btnPrevious,btnNext;
+    private ImageButton btnStart, btnStop, btnPrevious, btnNext;
 
     private TextView textMelody;
     private TextView textTimer;
@@ -27,15 +41,20 @@ public class MainActivity extends AppCompatActivity {
 
     private ObjectAnimator animation;
 
-    private int melodyIndex, defValue=0;
+    private int melodyIndex, defValue = 0;
 
     private int timer;
 
-    private SeekBar timerControl= null;
+    private SeekBar timerControl = null;
 
-    CountDownTimer cTimer=null;
+    CountDownTimer cTimer = null;
 
     SharedPreferences sPref;
+
+    private MediaPlayerService player;
+    boolean serviceBound = false;
+
+    ArrayList<Audio> audioList;
 
 
     @Override
@@ -44,48 +63,51 @@ public class MainActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-
-        sPref = getPreferences(MODE_PRIVATE);
-        melodyIndex = sPref.getInt("melody",0);
-        timer = sPref.getInt("timer",300000);
-
         //реклама
         // Load an ad into the AdMob banner view.
-        //AdView adView = (AdView) findViewById(adView);
-        //AdRequest adRequest = new AdRequest.Builder()
-        //        .addTestDevice("09D7B5315C60A80D280B8CDF618FD3DE")
-        //        .build();
-        //adView.loadAd(adRequest);
 
-        textMelody=(TextView) findViewById(R.id.textViewMelody);
-        textTimer=(TextView) findViewById(R.id.textViewTimer);
+        AdView adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("09D7B5315C60A80D280B8CDF618FD3DE")
+                .build();
+        adView.loadAd(adRequest);
 
 
-        data_melody=
+        sPref = getPreferences(MODE_PRIVATE);
+        melodyIndex = sPref.getInt("melody", 0);
+        timer = sPref.getInt("timer", 300000);
+
+
+
+        textMelody = (TextView) findViewById(R.id.textViewMelody);
+        textTimer = (TextView) findViewById(R.id.textViewTimer);
+
+
+        data_melody =
                 getResources().getStringArray(R.array.melody_array);
 
         textMelody.setText(data_melody[melodyIndex]);
 
-        textTimer.setText(timer/60000+ " мин.");
-
+        textTimer.setText(timer / 60000 + " мин.");
 
 
         timerControl = (SeekBar) findViewById(R.id.timer_bar);
-        timerControl.setProgress(timer/60000);
+        timerControl.setProgress(timer / 60000);
 
         timerControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (progress<5)
-                {progress=5;
-                    textTimer.setText(progress+ " мин.");}
-                else textTimer.setText(progress+ " мин.");
-                if (progress>175) {progress=600;
-                    textTimer.setText("∞ мин.");}
-                else textTimer.setText(progress+ " мин.");
+                if (progress < 5) {
+                    progress = 5;
+                    textTimer.setText(progress + " мин.");
+                } else textTimer.setText(progress + " мин.");
+                if (progress > 175) {
+                    progress = 600;
+                    textTimer.setText("∞ мин.");
+                } else textTimer.setText(progress + " мин.");
 
 
-                timer=progress*60000;
+                timer = progress * 60000;
 
             }
 
@@ -103,30 +125,30 @@ public class MainActivity extends AppCompatActivity {
 
         //анимация
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        animation = ObjectAnimator.ofInt (progressBar, "progress", 500, 0); //
+        animation = ObjectAnimator.ofInt(progressBar, "progress", 500, 0); //
         //animation.setDuration (30000); //in milliseconds
-        animation.setInterpolator (new LinearInterpolator());
+        animation.setInterpolator(new LinearInterpolator());
         //animation.start();
 
-       btnStart=(ImageButton) findViewById(R.id.btnStart);
+        btnStart = (ImageButton) findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 cancelTimer();
-                animation.setDuration (timer); //in milliseconds
+                animation.setDuration(timer); //in milliseconds
 
                 sPref = getPreferences(MODE_PRIVATE);
                 SharedPreferences.Editor ed = sPref.edit();
-                ed.putInt("melody",melodyIndex );
-                ed.putInt("timer",timer );
+                ed.putInt("melody", melodyIndex);
+                ed.putInt("timer", timer);
                 ed.commit();
-
+                playAudio(audioList.get(melodyIndex).getData());
                 startTimer(timer);
                 animation.start();
             }
         });
 
-        btnStop=(ImageButton) findViewById(R.id.btnStop);
+        btnStop = (ImageButton) findViewById(R.id.btnStop);
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -136,22 +158,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnPrevious=(ImageButton) findViewById(R.id.btnPrevious);
+        btnPrevious = (ImageButton) findViewById(R.id.btnPrevious);
         btnPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 melodyIndex--;
-                if (melodyIndex<0) melodyIndex=11;
+                if (melodyIndex < 0) melodyIndex = 11;
                 textMelody.setText(data_melody[melodyIndex]);
             }
         });
 
-        btnNext=(ImageButton) findViewById(R.id.btnNext);
+        btnNext = (ImageButton) findViewById(R.id.btnNext);
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 melodyIndex++;
-                if (melodyIndex>11) melodyIndex=0;
+                if (melodyIndex > 11) melodyIndex = 0;
                 textMelody.setText(data_melody[melodyIndex]);
             }
         });
@@ -161,29 +183,99 @@ public class MainActivity extends AppCompatActivity {
 
     void startTimer(int time) {
 
-        cTimer = new CountDownTimer(time,1000) {
+        cTimer = new CountDownTimer(time, 1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
-               //textTimer.setText("seconds remaining: " + millisUntilFinished / 1000);
-               // Toast.makeText(MainActivity.this,"seconds remaining: " + millisUntilFinished / 1000,
+                //textTimer.setText("seconds remaining: " + millisUntilFinished / 1000);
+                // Toast.makeText(MainActivity.this,"seconds remaining: " + millisUntilFinished / 1000,
                 //        Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onFinish() {
-              //  mTextField.setText("done!");
+                //  mTextField.setText("done!");
             }
 
         };
         cTimer.start();
-        animation.start ();
+        animation.start();
     }
 
-    void cancelTimer(){
-        if (cTimer!=null)
+    void cancelTimer() {
+        if (cTimer != null)
             cTimer.cancel();
 
 
+    }
+
+    //Binding this Client to the AudioPlayer Service
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            MediaPlayerService.LocalBinder binder = (MediaPlayerService.LocalBinder) service;
+            player = binder.getService();
+            serviceBound = true;
+
+            Toast.makeText(MainActivity.this, "Service Bound", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            serviceBound = false;
+        }
+    };
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean("ServiceState", serviceBound);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        serviceBound = savedInstanceState.getBoolean("ServiceState");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (serviceBound) {
+            unbindService(serviceConnection);
+            //service is active
+            player.stopSelf();
+        }
+    }
+
+    private void playAudio(int audioIndex) {
+        //Check is service is active
+        if (!serviceBound) {
+            //Store Serializable audioList to SharedPreferences
+           // StorageUtil storage = new StorageUtil(getApplicationContext());
+            //storage.storeAudio(audioList);
+           // storage.storeAudioIndex(audioIndex);
+
+
+
+
+            Intent playerIntent = new Intent(this, MediaPlayerService.class);
+            startService(playerIntent);
+            bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        } else {
+            //Store the new audioIndex to SharedPreferences
+           // StorageUtil storage = new StorageUtil(getApplicationContext());
+           // storage.storeAudioIndex(audioIndex);
+
+            //Service is active
+            //Send a broadcast to the service -> PLAY_NEW_AUDIO
+            sPref = getPreferences(MODE_PRIVATE);
+            SharedPreferences.Editor ed = sPref.edit();
+            ed.putInt("audioIndex", audioIndex);
+            ed.commit();
+            Intent broadcastIntent = new Intent(Broadcast_PLAY_NEW_AUDIO);
+            sendBroadcast(broadcastIntent);
+        }
     }
 }
